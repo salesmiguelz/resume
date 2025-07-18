1a9afb23b98288800c894879a097f1ed91220c8b
salesmiguelz <miguelsales02@gmail.com>
2025-07-18 17:18:06 -0300

    chore: rename styles files to .ts

---
+++ src/pages/Portfolio/components/Projects/components/Tag/styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+interface TagColorProps {
+    variant: 'blue' | 'red'
+}
+
+export const TagContainer = styled.div`
+    margin-top: 1rem;
+    display: flex;
+    background-color: red;
+    gap: 0.5rem;
+    background-color: ${props => props.theme['base-input']};
+    max-width: 12rem;
+    padding: 0.2rem 0.5rem;
+    border-radius: 0.625rem;
+`
+
+export const TagColor = styled.span<TagColorProps>`
+    height: 1rem;
+    width: 1rem;
+    background-color: ${props => props.variant === 'blue' ? props.theme['blue'] : 'red'};
+    border-radius: 50%;
+`
+
+export const TagLabel = styled.p`
+    color: ${props => props.theme['base-subtitle']};
+`
