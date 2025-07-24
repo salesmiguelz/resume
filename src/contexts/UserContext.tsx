dc0751b3ff0b7464dbd87fe581bef81eff678181
salesmiguelz <miguelsales02@gmail.com>
2025-07-24 20:32:48 -0300

    chore: log with api base url

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -88,6 +88,7 @@ export function UserContextProvider({ children }: UserContextProviderProps) {
     async function fetchUserData(username: string) {
         try {
             const response = await axios.get(`${API_BASE_URL}/users/${username}`);
+            console.log(`${API_BASE_URL}/users/${username}`)
             return {
                 statusCode: response.status,
                 data: response.data,
